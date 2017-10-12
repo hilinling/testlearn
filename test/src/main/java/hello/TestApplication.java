360c72d9c4c65bd3d12ddb8eacd489d4ec4d9d73
hilinling <hilinling@qq.com>
2017-10-12 16:41:55 +0800

    add

--- test/src/main/java/hello/TestApplication.java
+++ test/src/main/java/hello/TestApplication.java
@@ -1,99 +1,69 @@
 package hello;
 
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
 
 /**
  * Created by ling on 17/8/4.
  */
 public class TestApplication {
 
-    public static String decodeMaibiaoQrId(String qrScanId) {
-        String prefix = null, encodeId = null;
-        final int hyphenIndex = qrScanId.lastIndexOf('-');
-        final int idIndex = qrScanId.indexOf("&");
-        if (hyphenIndex != -1) {
-            prefix = qrScanId.substring(0, hyphenIndex + 1).toUpperCase();
-            encodeId = qrScanId.substring(hyphenIndex + 1,idIndex).toLowerCase();
-        }
-        if (prefix == null || encodeId.equals("") || encodeId.length() % 2 != 0) {
-            return null;
-        }
-
-        final String key = "18612521";
-        final List<Integer> secret = Stream.of(key.split("")).map(Integer::parseInt).collect(Collectors.toList());
-        final int originLen = encodeId.length();
-        final List<Character> encodeChars = new ArrayList<>();
-        final StringBuilder originChars = new StringBuilder();
-        for (int i = 0; i < originLen; i = i + 2) {
-            final String encodeHexTwoChar = encodeId.substring(i, i + 2);
-            final int encodeHexInt = Integer.parseInt(encodeHexTwoChar, 16);
-            encodeChars.add((char) encodeHexInt);
-        }
-        for (int i = 0; i < encodeChars.size(); i++) {
-            originChars.append((char) (encodeChars.get(i) ^ (secret.get(i % key.length()))));
-        }
-        return prefix + getMd5(originChars.toString()).toUpperCase();
-    }
-
-    /**
-     * md5 加密, 可选结果字符串长度, 默认 md5 是 32位, 这里如果 bitLength == 16, 则截取 32 位中间 16位返回.
-     */
-    public static String getMd5(String origin, Integer bitLength) {
-        final String md5Str = getMd5(origin);
-        if (bitLength != null && bitLength.equals(16)) {
-            return md5Str.substring(8, bitLength + 8);
-        } else {
-            return md5Str;
-        }
-    }
-
-    /**
-     * md5 加密.
-     */
-    public static String getMd5(String origin) {
-        if (origin == null) {
-            throw new IllegalArgumentException("digest origin string must not be null!");
-        }
-        try {
-            final MessageDigest digest = MessageDigest.getInstance("MD5");
-            final StringBuilder sb = new StringBuilder();
-            for (byte b : digest.digest(origin.getBytes())) {
-                sb.append(String.format("%02x", b));
-            }
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            throw new IllegalArgumentException("digest instance get error!");
-        }
-    }
-
-
     public static void main(String... args) {
-        Num a1 = new Num(2);
-        Num a2 = new Num(1);
-        Num a3 = new Num(5);
-        Num a4 = new Num(3);
-        List<Num> a = Arrays.asList(a1,a2,a3,a4);
-        Optional<Num> first = a.stream().sorted(Comparator.comparingInt(Num::getA).reversed()).findFirst();
-        System.out.println(first.get().getA());
+        String qrCodeText = "http://app.hw99lt.com/";
+        String filePath = "/";
+        int size = 125;
+        String fileType = "png";
+        File qrFile = new File(filePath);
+
+        try {
+            createQRImage(qrFile, qrCodeText, size, fileType);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+    private static void createQRImage(File qrFile, String qrCodeText, int size,
+                                      String fileType) throws WriterException, IOException {
+        // Create the ByteMatrix for the QR-Code that encodes the given String
+        Hashtable hintMap = new Hashtable();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText,
+                BarcodeFormat.QR_CODE, size, size, hintMap);
+        // Make the BufferedImage that are to hold the QRCode
+        int matrixWidth = byteMatrix.getWidth();
+        BufferedImage image = new BufferedImage(matrixWidth, matrixWidth,
+                BufferedImage.TYPE_INT_RGB);
+        image.createGraphics();
 
-    public static class Num {
-        private int a;
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+        // Paint and save the image using the ByteMatrix
+        graphics.setColor(Color.BLACK);
 
-        public Num(int a) {
-            this.a = a;
+        for (int i = 0; i < matrixWidth; i++) {
+            for (int j = 0; j < matrixWidth; j++) {
+                if (byteMatrix.get(i, j)) {
+                    graphics.fillRect(i, j, 1, 1);
+                }
+            }
         }
-
-        public int getA() {
-            return a;
-        }
+        ImageIO.write(image, fileType, qrFile);
     }
 
 
